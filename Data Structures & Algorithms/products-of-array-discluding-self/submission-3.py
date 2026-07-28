0be150f956250da0d771b08fb60df7636fddaf3b
CodeChamp95 <areeb.shaikh22@spit.ac.in>
2026-07-28 16:07:15 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans_list = []
+        first_value = 1
+        first_value_special = 1
+        array_map = {}
+
+        for i in range(len(nums)):
+            array_map[nums[i]] = array_map.get(nums[i],0) + 1
+
+        for i in range(1,len(nums)):
+            first_value *= nums[i]
+
+        if nums[0] == 0:
+            ans_list.append(first_value)
+            for i in range(1,len(nums)):
+                ans_list.append(0)
+            return ans_list
+
+        for i in range(1,len(nums)):
+            if nums[i] == 0:
+                continue
+            first_value_special *= nums[i]
+
+        ans_list.append(first_value)        
+        for i in range(1,len(nums)):
+            # array_map[nums[i]] = array_map.get(nums[i],0) - 1
+            if nums[i] == 0 and array_map[nums[i]] == 1:
+                res = first_value_special * nums[0]
+                ans_list.append(res)
+            elif nums[i] == 0 and array_map[nums[i]] > 1:
+                ans_list.append(0)
+            else:
+                res = (first_value/nums[i]) * nums[0]
+                ans_list.append(int(res))
+
+        return ans_list     
