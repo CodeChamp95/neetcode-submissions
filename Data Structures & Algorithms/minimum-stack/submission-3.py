f5c3b43691ed6e1557bf01f0685e1f4dfaa113d1
CodeChamp95 <areeb.shaikh22@spit.ac.in>
2026-08-09 23:45:40 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def __init__(self):
+        self.first = -1
+        self.lst = []
+        # self.min_number = 2^31
+
+    def push(self, val: int) -> None:
+        self.first += 1
+        self.lst.append(val)
+        # self.min_number = min(self.min_number, val)
+        
+    def pop(self) -> None:
+        if self.first != -1:
+            self.lst.pop(self.first)
+            self.first -= 1
+
+    def top(self) -> int:
+        return self.lst[self.first]
+        
+    def getMin(self) -> int:
+        # print("the stack is: ")
+        # self.display()
+        min_num = 2147483648
+        for element in self.lst:
+            min_num = min(min_num, element)
+        return min_num 
+
+    def display(self):
+        for i in range(self.first + 1):
+            print(self.lst[i])
+        
