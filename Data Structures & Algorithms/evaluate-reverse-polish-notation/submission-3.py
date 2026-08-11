ffcdc8964021bc82447607183aab4d107f28b2d4
CodeChamp95 <areeb.shaikh22@spit.ac.in>
2026-08-11 14:56:37 +0530

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        lst = []
+        top = -1
+        operators = "+-*/"
+        operator_set = set(operators)
+        res = 0
+        for token in tokens:
+            if token not in operator_set:
+                res = int(token)
+                lst.append(res)
+                top += 1
+            else:
+                # print("Entered operand section")
+                opnd1 = lst.pop()
+                top -= 1
+                opnd2 = lst.pop()
+                top -= 1
+
+                if token == "+":
+                    res = opnd1 + opnd2
+                    lst.append(res)
+                    top += 1
+                elif token == "-":
+                    res = opnd2 - opnd1
+                    lst.append(res)
+                    top += 1
+                elif token == "*":
+                    res = opnd1 * opnd2
+                    lst.append(res)
+                    top += 1
+                else:
+                    res = opnd2/opnd1
+                    lst.append(int(res))
+                    top += 1
+                print(f"result is {res}")
+        
+        return res
