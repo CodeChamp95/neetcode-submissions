b9c55c2316994a2d15382310795400c7cb404130
CodeChamp95 <areeb.shaikh22@spit.ac.in>
2026-08-18 16:18:40 +0530

    Add: merge-two-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-3.py
@@ -0,0 +1,62 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None and list2 is None:
+            return None
+        elif list1 is None and list2 is not None:
+            return list2
+        elif list2 is None and list1 is not None:
+            return list1
+
+        first_temp = list1
+        second_temp = list2
+        first_temp_prev = first_temp
+        count = 0
+        while first_temp != None and second_temp!= None:
+            if first_temp.val == second_temp.val:
+                prev_first_temp_next = first_temp.next
+                prev_second_temp_next = second_temp.next
+                first_temp.next = second_temp
+                second_temp.next = prev_first_temp_next
+                first_temp_prev = first_temp.next
+                first_temp = prev_first_temp_next
+                second_temp = prev_second_temp_next
+            elif first_temp.val < second_temp.val:
+                first_temp_prev = first_temp
+                first_temp = first_temp.next
+            else:
+                if first_temp == list1:
+                    if count == 0:
+                        list1 = second_temp
+                    prev_second_temp_next = second_temp.next
+                    # first_temp_prev.next = second_temp
+                    second_temp.next = first_temp
+                    first_temp_prev = second_temp
+                    # first_temp = first_temp.next
+                    second_temp = prev_second_temp_next
+                    count += 1
+                else:
+                    prev_second_temp_next = second_temp.next
+                    first_temp_prev.next = second_temp
+                    second_temp.next = first_temp
+                    first_temp_prev = first_temp
+                    first_temp = first_temp.next
+                    second_temp = prev_second_temp_next
+        
+        if first_temp == None:
+            while second_temp != None:
+                prev_second_temp_next = second_temp.next
+                first_temp_prev.next = second_temp
+                second_temp.next = None
+                second_temp = prev_second_temp_next
+                first_temp_prev = first_temp_prev.next
+        
+        return list1
+        
+
+        
